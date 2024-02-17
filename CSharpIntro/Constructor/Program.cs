ceb637796ea42171f02d3614ecceeed5e2185fc9
İsmet SAYIN <98154124+ismetsayin@users.noreply.github.com>
2024-02-17 22:22:24 +0300

    Add files via upload

---
+++ CSharpIntro/Constructor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "İsmet", LastName = "Sayın", City = "Konya" };
+            Customer customer2 = new Customer(2, "Salih", "Sayın", "Konya");
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+            Console.WriteLine(customer2.FirstName);
+
+            
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            
+        }
+
+        // Default constructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
